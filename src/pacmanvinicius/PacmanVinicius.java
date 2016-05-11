46ca6ae461b8624c6e5af881ea9d54ebe6aae3ae
Vinícius Duarte Lopes <vin.duartelopes.@gmail.com>
2016-05-11 14:26:23 -0300

    base inicial do Pacman do Vinicius

---
+++ src/pacmanvinicius/PacmanVinicius.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacmanvinicius;
+
+import java.awt.Graphics2D;
+
+/**
+ *
+ * @author viniciusduarte
+ */
+public class PacmanVinicius extends base.Jogo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        base.JogoApp.inicia(new PacmanVinicius());
+    }
+
+    @Override
+    public void inicia() {
+        
+    }
+
+    @Override
+    public void desenha(Graphics2D g) {
+        
+    }
+
+    @Override
+    public void atualiza() {
+        
+    }
+    
+}
